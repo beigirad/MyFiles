20257a9c7e0a2d1c303dd027c49a2a16a6ab6efd
beigirad <f.beigirad@gmail.com>
2019-04-17 13:21:40 +0430

    Initial commit

---
+++ MyFiles/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MyFiles
+//
+//  Created by Farhad BR on 4/8/19.
+//  Copyright © 2019 Farhad BR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let fileManager = FileManager.default
+        if let tDocumentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let filePath =  tDocumentDirectory.appendingPathComponent("Videos")
+            if !fileManager.fileExists(atPath: filePath.path) {
+                do {
+                    try fileManager.createDirectory(atPath: filePath.path, withIntermediateDirectories: true, attributes: nil)
+                } catch {
+                    NSLog("Couldn't create document directory")
+                }
+            }
+
+            NSLog("Document directory is \(filePath)")
+        }
+    }
+    
+
+
+}
+
